d5631d6279b12b37f6ed30d71b6e68aeac06d0bf
RBFZ <raabee12345@outlook.com>
2023-09-28 00:46:34 -0500

    *Added Background image *Edited all texts, entries and buttons to blend *Added frames for all buttons to be borderless

--- app.py
+++ app.py
@@ -1,61 +1,77 @@
 from tkinter import *
+from tkmacosx import Button
+from PIL import ImageTk, Image
 import requests #library needed
 from time import sleep #delay to ouput
 from tkinter.messagebox import _show
 
 root = Tk()
 root.title("~The Weather Checker~")
-root.geometry("1000x800")
+root.geometry("800x600")
+
+    
+
+#background image
+bg = ImageTk.PhotoImage(file="images/cloud_bg.png")
+
+my_label = Label(root, image=bg)
+my_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 #city Input
 def city_Click():
-    print_city = (f"You entered: {city_entry.get('1.0','end')}")
+    print_city = (f"{city_entry.get('1.0','end')}")
     city['text'] = str(print_city)
 
-city_label = Label(root, text="Enter a city: ")
-city_label.pack()
-city_entry = Text(root, height=1, width=15, bg="grey", fg="black", borderwidth=5)
+city_label = Label(root, text="Enter a city: ", bg="#53B5CE", fg="white")
+city_label.pack()                              #cursor color                 when out of focus          in focus
+city_entry = Text(root, height=1, width=15, insertbackground="cyan", highlightbackground="white", highlightcolor="yellow", bg="#53B5CE", fg="white")
 city_entry.pack()
 
-city_button = Button(root, width=20, text="Click after entering your city", command=city_Click)
+city_frame = Frame(root, bg="#53B5CE")
+city_frame.pack(pady=0)
+city_button = Button(city_frame, text="Click after entering your city", borderless=1, command=city_Click)
 city_button.pack()
 
-city = Label(root)
+city = Label(root, height=0, bg="#4FB3CA")
 city.pack()
 
 #State Input
-state_blank = Label(root, text="If a state code is not applicable, leave blank and click the button.")
+state_blank = Label(root, text="If a State Code is not applicable, leave blank and click the button.", bg="#4FB3CA")
 state_blank.pack()
 
-state_label = Label(root, text="Enter a State Code: (E.g. TX, AZ, etc.) ")
+state_label = Label(root, text="Enter a State Code:\n (E.g. TX, AZ, etc.) ", bg="#52B4CC", fg="white")
 state_label.pack()
-state_entry = Text(root, height=1, width=15, bg="grey", fg="black", borderwidth=5)
+state_entry = Text(root, height=0, width=15, insertbackground="cyan", highlightbackground="white", highlightcolor="yellow", bg="#53B5CE", fg="white")
 state_entry.pack()
 
 def state_Click():
-    print_state = (f"You entered: {state_entry.get('1.0','end')}")
+    print_state = (f"{state_entry.get('1.0','end')}")
     state['text'] = str(print_state)
 
-state_button = Button(root, width=20, text="Click after entering your State", command=state_Click)
+state_frame = Frame(root, bg="#53B5CE")
+state_frame.pack(pady=0)
+state_button = Button(state_frame, text="Click after entering your State", borderless=1, command=state_Click)
 state_button.pack()
 
-state = Label(root)
+state = Label(root, bg="#64BDD4")
 state.pack()
 
 #Country Input
-country_label = Label(root, text="Enter a Country Code: (E.g. USA, CA, etc.)")
+country_label = Label(root, text="Enter a Country Code:\n (E.g. USA, CA, etc.)", bg="#6FC4DB", fg="white")
 country_label.pack()
-country_entry = Text(root, height=1, width=15, bg="grey", fg="black", borderwidth=5)
+country_entry = Text(root, height=1, width=15, insertbackground="cyan", highlightbackground="white", highlightcolor="yellow", bg="#53B5CE", fg="white")
 country_entry.pack()
 
 def country_Click():
-    print_country = (f"You entered: {country_entry.get('1.0','end')}")
+    print_country = (f"{country_entry.get('1.0','end')}")
     country['text'] = str(print_country)
 
-country_button = Button(root, width=20, text="Click after entering your Country", command=country_Click)
+country_frame = Frame(root, bg="#53B5CE")
+country_frame.pack(pady=0)
+country_button = Button(country_frame, text="Click after entering your Country", borderless=1, command=country_Click)
 country_button.pack()
 
-country = Label(root)
+country = Label(root, bg="#83CFE4")
 country.pack()
 
 #input ends
@@ -89,10 +105,12 @@ def continue_Click():
     location_data = get_location.json()[0] #gets latitude and longitude
     lat = location_data['lat']
     lon = location_data['lon']
-    root.after(3000,lambda : _show("Success!", "Latitude and Longitude Values Have Been Recorded!\nPlease click the 'Obtain Weather Data' button next"))
+    root.after(3000,lambda : _show("Success!", "Latitude and Longitude Values Have Been Recorded!\nClick the 'Obtain Weather Data' button next"))
 
-done_button = Button(root, width=30, text="Click after you are finished entering information", command=continue_Click)
-done_button.pack()
+lonlat_frame = Frame(root, bg="#53B5CE")
+lonlat_frame.pack(pady=0)
+lonlat_button = Button(lonlat_frame, text="Click after you are finished entering information", borderless=1, command=continue_Click)
+lonlat_button.pack()
 
 #obtaining weather values from lat/lon location data
 
@@ -106,9 +124,11 @@ def weather_Click():
     temp = get_weather_data.json()['list'][0]['main']['temp']
     print_weather_data = Label(root, text="Obtaining Weather Data...")
     print_weather_data.pack()
-    root.after(2000,lambda : _show("Success!",  "Obtained Weather Data!\nPlease click the 'Print Weather Data' button next"))
+    root.after(2000,lambda : _show("Success!",  "Obtained Weather Data!\nClick the 'Print Weather Data' to print data"))
 
-get_weather_button = Button(root, width=30, text="Click to Obtain Weather Data", command=weather_Click)
+gw_frame = Frame(root, bg="#53B5CE")
+gw_frame.pack(pady=0)
+get_weather_button = Button(gw_frame, text="Click to Obtain Weather Data", borderless=1, command=weather_Click)
 get_weather_button.pack()
 
 #put lat and lon into request to get weather data
@@ -121,17 +141,17 @@ def print_weather_Click():
     cityname = city_entry.get('1.0','end')
     if weather == 'Clouds':
         root.after(1000,lambda : _show(f"Weather for {cityname}",  f"The skies in {cityname} are a bit shady with some {weather} in the skies.\nThe temperature in {cityname} is {temp} degrees"))
-        #print(f"The weather in {city} is a bit shady with some {weather} in the skies.")
     elif weather == 'Clear':
         root.after(1000,lambda : _show(f"Weather for {cityname}",  f"The weather in {cityname} might get sunny during the day with {weather} skies.\nThe temperature in {cityname} is {temp} degrees"))
-        #print(f"The weather in {city} is  outside with some {weather} skies")
     elif weather == 'Rain':
-        root.after(1000,lambda : _show(f"Weather for {cityname}",  f"The weather in {cityname} is a bit damp today as it has been raining.\nThe temperature in {cityname} is {temp} degrees"))
+        root.after(1000,lambda : _show(f"Weather for {cityname}",  f"The weather in {cityname} is a bit damp today as it has rained recently.\nThe temperature in {cityname} is {temp} degrees"))
     else:
         root.after(1000,lambda : _show(f"Weather for {cityname}",  f"Weather in {cityname}: {weather}.\nThe temperature in {cityname} is {temp} degrees"))
     root.after(6000, root.destroy)
 
-print_weather_button = Button(root, width=30, text="Print Weather Data", command=print_weather_Click)
+pw_frame = Frame(root, bg="#53B5CE")
+pw_frame.pack(pady=0)
+print_weather_button = Button(pw_frame, text="Print Weather Data", borderless=1, command=print_weather_Click)
 print_weather_button.pack()
 
 root.mainloop()
